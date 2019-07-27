52d63c2e5f20f2a827ab4350da9f1bdc67aaae08
Khoa Pham <ppkhoa@users.noreply.github.com>
2019-07-27 20:24:17 +0700

    Test signing commits

--- ddn-config-backup.sh
+++ ddn-config-backup.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 # This script will backup network and Linux configuration in preparation for
-# GRIDScaler 4.x upgrade/reinstall or EXAScaler 3.0 upgrade/reinstall.
+# GRIDScaler 4.x upgrade/reinstall or EXAScaler 3.x/4.x upgrade/reinstall.
 # This script is meant to run on each GRIDScaler/EXAScaler server.
 # The current version of the script supports the standard GS/ES installation, 
 # it does not back up configurations for applications not included with original GS/ES installation.
 
 # For support/feedback with the script, contact Khoa Pham (kpham@ddn.com).
 
-# Changelogs has been moved to GitHub wiki: https://github.com/ppkhoa/ddn-config-backup/wiki
+# Changelogs has been moved to GitHub wiki: https://github.com/ppkhoa/ddn-config-backup/wiki 
 
 
 ###############################################################################
